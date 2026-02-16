5bfcf52144151fe9e411b5d5b2721fef41f6cf0d
tom34230 <tomprz34230@gmail.com>
2026-02-16 10:35:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,9 +120,8 @@ async def scan_loop():
 @client.event
 async def on_ready():
     print(f"Bot connecté en tant que {client.user}")
-    
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send("✅ BOT OPÉRATIONNEL")
-    
-    client.loop.create_task(scan_loop())
 
+    channel = await client.fetch_channel(CHANNEL_ID)
+    await channel.send("✅ BOT OPÉRATIONNEL")
+
+    client.loop.create_task(scan_loop())
